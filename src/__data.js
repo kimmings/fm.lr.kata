4cc6bc3dd0c9bd84364a2cd7322db3a590ba0fdb
Julian Kimmings <julian@flat-media.com>
2019-03-09 12:47:24 +0000

    Tidy Up create-react-app. Add data. Add react-bootstrap. Render table.

---
+++ src/__data.js
@@ -0,0 +1,20 @@
+export default  [
+    {
+        "id": "abc",
+        "name": "hotelone",
+        "starRating": 5,
+        "facilities": ["car park", "pool"]
+    },
+    {
+        "id": "def",
+        "name": "hoteltwo",
+        "starRating": 3,
+        "facilities": ["car park", "gym"]
+    },
+    {
+        "id": "ghi",
+        "name": "hotelthree",
+        "starRating": 3,
+        "facilities": []
+    }
+];
